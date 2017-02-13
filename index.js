6589404fee59b31c1adb62a28984c4dd2eaf074d
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-13 09:04:08 -0500

    Moved 'read' and 'write' functions into dedicated partial/lib. files, use 'index.js' as 'manifest'.

--- index.js
+++ index.js
@@ -1,51 +1,8 @@
 // --------------------------------------------------
-// IMPORT MODULES
+// IMPORT LIB. FUNCTIONS
 // --------------------------------------------------
-var fs = require( 'fs' );
-var Promise = require( 'bluebird' );
-
-
-// --------------------------------------------------
-// DECLARE FUNCTIONS
-// --------------------------------------------------
-/**
- * ...
- */
-function read( path ) {
-    if ( !path ) {
-        return Promise.reject( null );
-    }
-
-    return new Promise( function( resolve, reject ) {
-        fs.readFile( path, function( err, data ) {
-            if ( err ) {
-                reject( err );
-            }
-
-            resolve( data );
-        } )
-    } );
-}
-
-
-/**
- * ...
- */
-function write( path, data ) {
-    if ( !path || !data ) {
-        return Promise.reject( null );
-    }
-
-    return new Promise( function( resolve, reject ) {
-        fs.writeFile( path, data, function( err ) {
-            if ( err ) {
-                reject( err );
-            }
-
-            resolve( true );
-        } );
-    } );
-}
+var read = require( './lib/read' );
+var write = require( './lib/write' );
 
 
 // --------------------------------------------------
